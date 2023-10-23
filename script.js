bc71038b6744bfd04ed611c050ead0c9140c2eb5
Raima Aftab <raima.aftab2022@gmail.com>
2023-10-23 20:19:42 +0500

    bmi calculator
    bmi calculator ui and functionality with validations

---
+++ script.js
@@ -0,0 +1,45 @@
+function calculateBMI() 
+{
+    const weight = parseFloat(document.getElementById("weight").value);
+    const feet = parseFloat(document.getElementById("heightFeet").value);
+    const inches = parseFloat(document.getElementById("heightInches").value);
+    const errorMessage = document.getElementById("error");
+ console.log(weight);
+ console.log( feet);
+ console.log(inches);
+    //checks if any of the user input values (weight, feet, or inches) is not a valid number (e.g text or empty input fields)
+    if (isNaN(weight) || isNaN(feet) || isNaN(inches)|| weight === 0 || feet === 0)
+     {
+        errorMessage.textContent="Please enter all fields with avalid number";
+        return;
+    }
+    // 1 foot is equal to 0.3048 meters
+    //1 inch is equal to 0.0254 meters
+    const heightMeters = (feet * 0.3048) + (inches * 0.0254);
+    const bmi = weight / (heightMeters ** 2);
+
+    let status = "";
+    //let color = "";
+    // clear error message
+    errorMessage.textContent="";
+
+    if (bmi <= 18.4) 
+    {
+        status = "UnderWeight";
+    } 
+     else if (bmi >= 18.5 && bmi < 25) 
+     {
+        status = "Normal";
+    } 
+    else if (bmi >= 25 && bmi <= 39.9) 
+    {
+        status = "Overweight";
+    } 
+    if (bmi >= 40)
+     {
+        status = "Obese"; 
+    } 
+    // toFixed(1) method is used to format the BMI to two decimal places, ensuring it's displayed with two digits after the decimal point.
+
+    document.getElementById("result").innerHTML = `Your BMI is: ${bmi.toFixed(1)} (This is considerd as ${status})`;
+}
