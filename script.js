ec03d28575bf69c50a96f3aa02248bc6d3e721c5
Raima Aftab <raima.aftab2022@gmail.com>
2023-10-24 09:22:22 +0500

    chart
    add status chart

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function calculateBMI() 
 {
+    // parseFloat converts a string into a floating point
     const weight = parseFloat(document.getElementById("weight").value);
     const feet = parseFloat(document.getElementById("heightFeet").value);
     const inches = parseFloat(document.getElementById("heightInches").value);
@@ -7,7 +8,7 @@ function calculateBMI()
  console.log(weight);
  console.log( feet);
  console.log(inches);
-    //checks if any of the user input values (weight, feet, or inches) is not a valid number (e.g text or empty input fields)
+    //checks if any of the user input values (weight,feet,inches) is not a valid number (text/empty input fields)
     if (isNaN(weight) || isNaN(feet) || isNaN(inches)|| weight === 0 || feet === 0)
      {
         errorMessage.textContent="Please enter all fields with avalid number";
@@ -18,28 +19,31 @@ function calculateBMI()
     const heightMeters = (feet * 0.3048) + (inches * 0.0254);
     const bmi = weight / (heightMeters ** 2);
 
+    // initiallizing status
     let status = "";
-    //let color = "";
+ 
     // clear error message
     errorMessage.textContent="";
 
     if (bmi <= 18.4) 
     {
-        status = "UnderWeight";
+        status="UnderWeight";
     } 
-     else if (bmi >= 18.5 && bmi < 25) 
+     else
+    if (bmi >= 18.5 && bmi <= 24.9) 
      {
-        status = "Normal";
+        status ="Normal";
     } 
-    else if (bmi >= 25 && bmi <= 39.9) 
+    else
+    if (bmi >= 25 && bmi <= 39.9) 
     {
-        status = "Overweight";
+        status ="Overweight";
     } 
     if (bmi >= 40)
      {
         status = "Obese"; 
     } 
-    // toFixed(1) method is used to format the BMI to two decimal places, ensuring it's displayed with two digits after the decimal point.
+    // toFixed(1) method is used to format the BMI to two decimal places, ensuring it's displayed with one digit after the decimal point.
 
-    document.getElementById("result").innerHTML = `Your BMI is: ${bmi.toFixed(1)} (This is considerd as ${status})`;
+    document.getElementById("result").innerHTML = `Your BMI is:(${bmi.toFixed(1)})  .This is considerd as ${status}.`;
 }
